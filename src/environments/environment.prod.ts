fc298c9053169cf3482f53dcf79d2d32f3896390
chaluemwuut <chaluemwut@gmail.com>
2022-08-28 14:36:54 +0700

    add aws https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL:'http://dev-sw6-uapi.ecm.in.th/uapi/drt-ElectronicsDocument'
+  apiURL:'https://dev-sw6-uapi.ecm.in.th/uapi/drt-ElectronicsDocument'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL:'http://dev-sw6-uapi.ecm.in.th/uapi/drt-ElectronicsDocument'
+  apiURL:'https://dev-sw6-uapi.ecm.in.th/uapi/drt-ElectronicsDocument'
 };
 
 /*
